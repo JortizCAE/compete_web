7eeee4592c20d46ed9a1df56420b2fdfb4abcd36
Ali Imran <ali36766@gmail.com>
2022-07-19 17:44:03 +0500

    Updated Static Address in Progress

--- src/pages/Progress/Progress.jsx
+++ src/pages/Progress/Progress.jsx
@@ -3,7 +3,7 @@ import Iframe from "react-iframe";
 export default function MyProgress() {
   return (
     <Iframe
-      url="https://books-hub.vercel.app/"
+      url="http://35.83.209.174:9001"
       width="100%"
       height="100%"
       id="myId"
